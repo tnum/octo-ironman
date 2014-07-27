0dcc3196a3a34600a98ccf39a1887bed7ab66e2b
tnum <t.chudha@gmail.com>
2014-07-27 19:29:57 +0100

    created welcome controller with index and about views

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+# The welcome_controller is a Ruby class
+# Contains two empty methods which correspond to the view names
+
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+end

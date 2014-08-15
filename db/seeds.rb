58f9da3f0b5ac18a12e049391b601f9efa4703ab
tnum <t.chudha@gmail.com>
2014-08-15 22:05:21 +0100

    Associated and nested topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,20 @@ require 'faker'
 end
 users = User.all
 
+# Create Topics
+15.times do
+  Topic.create(
+    name:   Faker::Lorem.sentence, 
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do 
   Post.create(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -32,10 +42,34 @@ posts = Post.all
     )
 end
 
-User.first.update_attributes(
-  email: 't.applegate@susumu.co.uk',
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
   password: 'helloworld',
+  role:     'admin'
 )
+admin.skip_confirmation!
+admin.save
+
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com', 
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+member.skip_confirmation!
+member.save
 
 puts "Seed finished"
 puts "#{Post.count} posts created"

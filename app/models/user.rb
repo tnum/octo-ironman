96a7a84d9144e9a9b3ded50248b43f217a6d52ed
tnum <t.chudha@gmail.com>
2014-08-15 18:48:04 +0100

    add pundit for authrorisation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
 
+  # associations
   has_many :posts
+
+  # validations
+
+  # plugins
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  # everything else
+  def role?(base_role)
+    role == base_role.to_s
+  end
 end

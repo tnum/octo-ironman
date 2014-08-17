8f7fa0730e754ae135ec4d45255317451b78d732
tnum <t.chudha@gmail.com>
2014-08-17 21:01:38 +0100

    validating posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def my_name
     "Teenum"
   end
+
+  def form_group_tag(errors, &block)
+
+    # Depending on whether ther errors array contains any weeoes, the content_tag method is called
+    # The form content_tag method builds the HTML to display the form element & its associated errors
+
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
+
 end

88fd444c5352b0becc7271187f7c31226613712c
tnum <t.chudha@gmail.com>
2014-10-10 21:29:14 +0100

    vote posts up and down

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,41 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :votes, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
   mount_uploader :image, ImageUploader
+
+  after_create :create_vote
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    new_rank = points + age
+
+    update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
+  # votes in the above code is an implied self.votes
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
 end
